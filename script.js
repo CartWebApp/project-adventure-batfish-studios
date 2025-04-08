8393b9917e0918fa3bca43bbcc5dd3edc5e6fd97
Aarow <aarow0021@gmail.com>
2025-04-08 06:44:01 -0700

    added text style support

--- script.js
+++ script.js
@@ -94,53 +94,89 @@ class Room {
     }
 }
 
-// parses text for color identifiers, returning clean text and a dictionary of location + color identifiers
-// example color identifier: [c:#0011] for green, or [c:] to reset
-function parseColors(text) {
+// parses text for identifiers, returning clean text and a dictionary of location + identifier values
+function parseText(text, identifier) {
     let data = [];
-    while (text.includes('[c:')) {
-        let index = text.indexOf('[c:');
-        let match = text.match(/(\[c:)([^\]]*)(\])/);
-        let color = match[2];
-        data.push({index, color});
-        text = text.substring(0, index) + text.substring(index + match[0].length)
+    let cleanText = text;
+    let specialMatches = [...text.matchAll(new RegExp(String.raw`(\[)(?!${identifier}:)([^:^\]]*:)([^\]]*)(\])`, 'g'))];
+    for (const match of specialMatches.reverse()) {
+        let index = match.index;
+        cleanText = cleanText.substring(0, index) + cleanText.substring(index + match[0].length)
     }
-    return {text, data}
+    while (cleanText.includes(`[${identifier}:`)) {
+        let match = cleanText.match(new RegExp(String.raw`(\[)(?=${identifier || ':'})(${identifier}:)([^\]]*)(\])`));
+        let index = match.index;
+        let value = match[3];
+        data.push({index, value});
+        cleanText = cleanText.substring(0, index) + cleanText.substring(index + match[0].length)
+    }
+    return {text: cleanText, data}
 }
 
+
 // returns an element with color formatted text
-function formatText(text, colorData) {
-    colorData = colorData ?? parseColors(text);
-    let colorDataIndex = 0;
+function formatText(text) {
+    //to add a style, just put a valid css style in the name and add an identifier
+    let textStyles = [
+        {name: 'reset', identifier: ''}, // parses for full style resets (removes all styles). Syntax is [-:]
+        {name: 'color', identifier: 'c'}, // example color identifier: [c:#0011] for green, or [c:] to reset
+        {name: 'fontFamily', identifier: 'ff'}, // example font families identifier: [ff:'Courier New'], or [ff:] to reset
+        {name: 'fontSize', identifier: 'fs'}, // example font sizes identifier: [fs:32px], or [fs:] to reset
+        {name: 'rotate', identifier: 'rt'}, // example rotation identifier: [rt:180deg], or [rt:] to reset
+        {name: 'textShadow', identifier: 'ts'}, // example text shadow identifier: [ts:4px,4px,3px,yellow], or [ts:] to reset
+    ]
+
+    for (const style of textStyles) {
+        let parsedData = parseText(text, style.identifier);
+        style.data = parsedData.data;
+        style.dataIndex = 0;
+        style.currentValue = style.currentValue ?? '';
+    }
+
+    let cleanText = parseText(text, 'This returns the clean text because nothing matches this.').text;
     characterIndex = 0;
     let formattedElement = document.createElement('span');
     formattedElement.className = 'finished-text';
-    let currentColor = '';
-    let wordArray = colorData.text.split(' ')
+    let wordArray = cleanText.split(' ')
     for (let word of wordArray) {
         let wordSpan = document.createElement('span')
         wordSpan.className = 'transition-word';
         formattedElement.appendChild(wordSpan)
         for (let char of word) {
-            if (characterIndex === colorData.data[colorDataIndex]?.index) {
-                currentColor = colorData.data[colorDataIndex].color;
-                colorDataIndex += 1;
-            }
             let charSpan = document.createElement('span');
             charSpan.textContent = char;
             charSpan.className = `transition-character`;
-            charSpan.style.color = currentColor;
+            for (const style of textStyles) {
+                if (characterIndex === style.data[style.dataIndex]?.index) {
+                    style.currentValue = style.data[style.dataIndex].value;
+                    style.dataIndex += 1;
+                    if (style.name === 'reset') {
+                        for (const style of textStyles) {
+                            style.currentValue = '';
+                        }
+                    }
+                }
+                charSpan.style[style.name] = style.currentValue;
+            }
             wordSpan.appendChild(charSpan);
             characterIndex++;
         }
         let space = document.createElement('span');
         space.textContent = ' ';
         space.className = `transition-character`;
-        wordSpan.appendChild(space);
-        if (characterIndex === colorData.data[colorDataIndex]?.index) {
-            currentColor = colorData.data[colorDataIndex].color;
-            colorDataIndex += 1;
+        for (const style of textStyles) {
+            if (characterIndex === style.data[style.dataIndex]?.index) {
+                style.currentValue = style.data[style.dataIndex].value;
+                style.dataIndex += 1;
+                if (style.name === 'reset') {
+                    for (const style of textStyles) {
+                        style.currentValue = '';
+                    }
+                }
+            }
+            space.style[style.name] = style.currentValue;
         }
+        wordSpan.appendChild(space);
         characterIndex++;
     }
     return formattedElement;
@@ -215,13 +251,14 @@ function init() {
     player = new Player();
     let room = new Room('Example');
 
-    // add colors to text by doing [c: + any valid css color + ]. To reset color, just do [c:]
+    // add styles to text by doing [identifier: + any valid css color + ]
+    // to reset that style, just do [identifier:]. to reset all styles, do [-:]
     // EX: [c:red] = [c:#ff0000] = [c:rgb(255,0,0)]
     room.addStory(`This is a [c:red]test[c:] story`);
-    room.addStory(`This is a [c:red]test[c:] story [c:#00ff00]continued`, 100, 33, 'impact');
-    room.addStory(`[c:#c5c5c5]Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`, 10, 3, 'funky');
+    room.addStory(`This is a [c:red]test[c:] story [c:#00ff00][ff:'Courier New'][fs:32px]contin[:]ued`, 100, 33, 'impact');
+    room.addStory(`[ts:2px 2px 2px white][c:#c5c5c5]Lorem [rt:90deg]ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et [rt:180deg]dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure [rt:270deg]dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui [rt:]officia deserunt mollit anim id est laborum.`, 10, 3, 'funky');
     room.addStory(`Woah`, 1000, 100, 'shaky');
-    room.addStory(`[c:rgb(0,255,255)]Cooleo![c:] This is a neat blur effect!`, 100, 10, 'blur');
+    room.addStory(`[c:rgb(0,255,255)]Cooleo![c:] This is a neat blur effect! I like it so much, I think I will put [c:yellow][fs:24px]more[:] text!`, 100, 10, 'blur');
     room.addStory(`Or maybe try [c:rgb(136, 255, 0)]a[c:rgb(0, 255, 98)]l[c:rgb(136, 255, 0)]t[c:rgb(0, 255, 98)]e[c:rgb(136, 255, 0)]r[c:rgb(0, 255, 98)]n[c:rgb(136, 255, 0)]a[c:rgb(0, 255, 98)]t[c:rgb(136, 255, 0)]i[c:rgb(0, 255, 98)]n[c:rgb(136, 255, 0)]g[c:] text? This can do that too! Lets see how this looks like when it's long: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`, 50, 10, 'fade-alternate');
     room.choices.push({
         name: 'Example choice',
